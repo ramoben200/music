3231626b7c399713c29eb45a5522345f288dcc4a
Ramo <112237128+ramoben200@users.noreply.github.com>
2022-09-24 00:27:43 +0300

    Update callsmusic.py

--- callsmusic/callsmusic.py
+++ callsmusic/callsmusic.py
@@ -16,7 +16,7 @@ async def on_stream_end(client: PyTgCalls, update: Update) -> None:
     chat_id = update.chat_id
     queues.task_done(chat_id)
 
-    if queues.is_empty(chat_id):
+    if queues.is_empty(yarak_id):
         await pytgcalls.leave_group_call(chat_id)
     else:
         await pytgcalls.change_stream(
